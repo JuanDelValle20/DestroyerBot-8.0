578b2cfc835c6ffd6cebb92977324883d5414fb0
JuanDelValle18 <jdelvalle585@gmail.com>
2021-08-19 13:47:44 -0600

    Update juegos.js

--- src/juegos.js
+++ src/juegos.js
@@ -2,18 +2,13 @@ const juegos = (prefix, pushname) => {
     return `
 *Comandos De Juegos 👾*
  
-${prefix}rankgay
-Conoce a el top de los mas gays del grupo
-
-${prefix}gay
-Conoce que tan gay eres 😎
 
 ${prefix}cuties
 Prueba tu suerte en el medidor de fan de cuties 😱
 
 _*En futuras versiones colocare mas juegos :)*_
      
-ву ѕнαη∂υу`
+ву Juan del Valle`
 
 }
 
