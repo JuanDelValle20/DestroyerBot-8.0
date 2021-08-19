a8bee19667e9750d599dbae76581c5d2196f9b33
JuanDelValle18 <86022076+JuanDelValle20@users.noreply.github.com>
2021-08-19 17:15:12 -0600

    Update shantera.js

--- src/shantera.js
+++ src/shantera.js
@@ -2,36 +2,31 @@ const shantera = (prefix, pushname) => {
     return `*Palabras especificas para que el bot interactue con ustedes*
 
 
-    - todo bien
-    - buenos dias
-    - bot malo
-    - gracias bot
-    - hola
-    - fua
-    - infoBot
-    - Cuánto es 12+1
-    - El bot sabe qué número va después del 12
-    - Fly
-    - Buenas noches
-    - Nidos de Arceus
-    - Ya están los nidos
-    - Me salió shiny
-    - Cómo funciona el bot
-    - Qué hay bot
-    - El bot habla
-    - Saca las caguamas
-    - Bot bueno
-    - 
-    - 
-    - 
+- todo bien bot
+- Buenos días
+- Bot malo
+- Bot bueno
+- gracias bot
+- Hola  
+- fua
+- infoBot
+- saca las caguamas
+- Saca las caguamas
+- El bot habla
+- qué hay bot
+- como funciona el bot
+- Me salió shiny
+- Ya están los nidos
+- Nidos de Arceus
+- Buenas noches
+- Fly 
+- El bot sabe qué número va después del 12 
+- Cuánto es 12+1
 
-    
-    
-    
-    _Ojito escribe tal y como esta en el mensaje_
-    _Nuevas palabras próximamente_
-    
-    by JuandelValle`
+
+_*Ojito escribe tal y como esta en el mensaje*_
+
+ву Juan del Valle`
 
 }
 
