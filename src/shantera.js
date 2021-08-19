a6e67adeb5e3d23781017c70c16755b229aa9dfa
JuanDelValle18 <jdelvalle585@gmail.com>
2021-08-19 13:48:02 -0600

    Update shantera.js

--- src/shantera.js
+++ src/shantera.js
@@ -2,49 +2,36 @@ const shantera = (prefix, pushname) => {
     return `*Palabras especificas para que el bot interactue con ustedes*
 
 
-Fiesta del admin
-Admin party
-Viernes
-GOOOOD
-Alto temazo
-Todo bien
-Buenos dias
-Bot gay
-Gracias
-Hola
-Fua
-Corte
-Gaspi buenos dias 
-Gaspi me saludas
-Gaspi y las minitas
-Gaspi todo bien
-Me quiero suicidar
-Gaspi ya no aguanto
-Contate algo bot
-Sexo
-Momento epico
-El bot del orto no funciona
-Epicardo
-Insta de la minita
-Una mierda de bot
-Ultimo momento
-Nefasto
-Paraguayo
-Bot de mierda
-Venezolano
-Gaspi corte
-Ya me voy a dormir
-Calefon
-Apurate bot
-Un chino
-No funciona
-Boliviano
-Enano
+    - todo bien
+    - buenos dias
+    - bot malo
+    - gracias bot
+    - hola
+    - fua
+    - infoBot
+    - Cuánto es 12+1
+    - El bot sabe qué número va después del 12
+    - Fly
+    - Buenas noches
+    - Nidos de Arceus
+    - Ya están los nidos
+    - Me salió shiny
+    - Cómo funciona el bot
+    - Qué hay bot
+    - El bot habla
+    - Saca las caguamas
+    - Bot bueno
+    - 
+    - 
+    - 
 
-
-_*Ojito escribe tal y como esta en el mensaje*_
-
-ву ѕнαη∂υу`
+    
+    
+    
+    _Ojito escribe tal y como esta en el mensaje_
+    _Nuevas palabras próximamente_
+    
+    by JuandelValle`
 
 }
 
