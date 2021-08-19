363794fc22bb9c823581f7cdca0fd8df037a3583
JuanDelValle18 <jdelvalle585@gmail.com>
2021-08-19 13:48:10 -0600

    Update version.js

--- src/version.js
+++ src/version.js
@@ -1,25 +1,13 @@
 const version = (prefix, pushname) => {
     return `
-*ѕнαηвσт ву ѕнαη∂υу*
+*DestroyerBot ву Juan del Valle*
 
 *Actualizado:* 18 de Agosto del 2021
-*Versión actual:* 3.7
-*Ofrecida por:* ѕнαη∂υу™
+*Versión actual:* 8.0
+*Ofrecida por:* Juan del Valle*
 
 
-*INFORME*
-
-Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
-Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
-Cambiare la apikey pasando un mes o si veo muchos atercados o robos
-Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
-
-
-*COMO ACTUALIZAR*
-
-https://cutt.ly/jmSF2RN
-
-ву ѕнαη∂υу
+ву Juan del Valle
 `
 
 }
