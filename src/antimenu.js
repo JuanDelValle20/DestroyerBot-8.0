e2cbc042d74fe49c5a95c923052ec63022a704ad
JuanDelValle18 <jdelvalle585@gmail.com>
2021-08-19 13:48:15 -0600

    Update antimenu.js

--- src/antimenu.js
+++ src/antimenu.js
@@ -68,7 +68,7 @@ ${prefix}antikwai 0
 _Para activar estos comandos el bot necesita tener admin en el grupo y esta funcion solo la activan los admins del grupo_
 
 
-ву ѕнαη∂υу
+ву Juan del Valle
 `
 
 }
